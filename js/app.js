ceff0958c7e705f3d98327e4c25a72c441e97073
lizz <elizabeth.ann.soup@gmail.com>
2020-04-01 12:40:52 -0400

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,13 @@
-(function() {
-    const button = document.querySelector('#btn')
-    const body = document.querySelector('body')
-    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    const value = document.querySelector('#hex-value')
+const body = document.querySelector('body');
+const button = document.getElementById('btn');
+const copy = document.getElementById('hex-value');
+// colors legend [0:brown, 1:grey, 2:pink, 3:purple, 4:blue, 5:turquoise, 6:green, 7:yellow, 8:gold]
+const colors = ['#A1887F', '#B0BEC5', '#FFCDD2', '#FFCDD2', '#D1C4E9', '#BBDEFB', '#B2EBF2', '#DCEDC8', '#FFF9C4', '#FFECB3'];
 
-    button.addEventListener('click', changeHex)
+button.addEventListener('click', changeHex);
 
-    function changeHex(){
-        let hex = '#'
-
-        for (let i = 0; i < 6; i++){
-            const index = Math.floor(Math.random()*hexValues.length)
-            hex += hexValues[index]
-        }
-        value.textContent = hex
-        body.style.backgroundColor = hex
-    }
-} )()
+function changeHex() {
+  let randomNumber = parseInt(Math.random() * colors.length);
+  body.style.background = colors[randomNumber];
+  copy.innerHTML = ` ${colors[randomNumber]}`;
+}
